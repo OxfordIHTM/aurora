ff5f5577a4f1b1d06767e58bbd711b205423a882
Ernest Guevarra <ernest@guevarra.io>
2024-04-05 20:14:46 +0100

    add structuring functions

--- R/ora_structure.R
+++ R/ora_structure.R
@@ -1,28 +1,78 @@
 #'
 #' Structure ORA XML outputs
 #'
-#' @param ora_xml XML outputs
+#' @param ora_xml XML outputs.
 #'
-#' @return A structured output
+#' @return A tibble outputs.
 #'
 #' @examples
-#' #ora_identify() |> ora_identify_structure()
+#' ora_identify()
+#' ora_list_meta_formats()
 #'
 #' @rdname ora_structure
 #' @export
 #'
 
 ora_structure_identify <- function(ora_xml) {
-  field_names <- ora_xml |>
-    xml2::xml_children() |>
-    xml2::xml_children() |>
-    xml2::xml_name()
+  xml_list <- xml2::as_list(ora_xml)
 
-  values <- ora_xml |>
-    xml2::xml_children() |>
-    xml2::xml_attrs(".//verb")
-    xml2::xml_children() |>
-    xml2::xml_contents() |>
-    xml2::xml_children() |>
-    xml2::xml_contents()
+  responseDate <- xml_list |>
+    unlist(recursive = FALSE) |>
+    (\(x) x[[1]][[1]])()
+
+  request <- xml_list[[1]][2] |>
+    (\(x) paste0(x$request, "?verb=", attributes(x$request)))()
+
+  description <- xml_list |>
+    unlist(recursive = FALSE) |>
+    (\(x) x[[3]][8])() |>
+    unlist() |>
+    dplyr::bind_rows()
+
+  names(description) <- names(description) |>
+    tools::file_ext()
+
+  tibble::tibble(
+    responseDate,
+    request,
+    xml_list |>
+      unlist(recursive = FALSE) |>
+    (\(x) x[[3]][1:7] |> unlist() |> dplyr::bind_rows())(),
+    description
+  )
+}
+
+#'
+#' @rdname ora_structure
+#' @export
+#'
+
+ora_structure_meta_formats <- function(ora_xml) {
+  xml_list <- ora_xml |>
+    xml2::as_list() |>
+    unlist(recursive = FALSE)
+
+  xml_list[1:2] <- xml_list[1:2] |>
+    unlist(recursive = FALSE)
+
+  xml_list[2] <- paste0(
+    xml_list[2],
+    "?verb=",
+    ora_xml |>
+      xml2::as_list() |>
+      (\(x) attributes(x[[1]][2][[1]]))()
+  )
+
+  names(xml_list) <- names(xml_list) |>
+    tools::file_ext()
+
+  xml_list[[3]] <- xml_list[[3]] |>
+    dplyr::bind_rows() |>
+    tidyr::unnest(
+      cols = c(
+        .data$metadataPrefix, .data$schema, .data$metadataNamespace
+      )
+    )
+
+  dplyr::bind_cols(xml_list)
 }
